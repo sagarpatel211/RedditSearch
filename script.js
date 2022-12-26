b9fa3c60191ff64e551cdcffa36a7a6434cf201e
Sagar Patel <2sagarpatel2@gmail.com>
2022-12-26 16:38:21 -0500

    Final commit! Project complete! V1

--- script.js
+++ script.js
@@ -1,74 +1,99 @@
-//  "https://www.googleapis.com/customsearch/v1?key=" + this.api_key + "-U&cx=" + this.cx + "&q=" + query
-
 let redditsearch = {
     api_key: "AIzaSyAtfq_gwRicOPODzk80kNcRuzVv3govv",
     cx: "e4d7867e22b0942fe",
     not_first_search : false,
     fetchResults: function (query) {
-        fetch("test.json").then((response) => response.json()).then((data) => this.displayResults(data));
+        fetch("https://www.googleapis.com/customsearch/v1?key=" + this.api_key + "-U&cx=" + this.cx + "&q=" + query
+        ).then((response) => response.json()).then((data) => this.displayResults(data));
     },
     RedditDescription: async function (url) {
-        const json_url = url + ".json";
-        const subreddit = await fetch(json_url).then((response) => response.json()).then((data) => data[0].data.children[0].data.subreddit);
-        const title = await fetch(json_url).then((response) => response.json()).then((data) => data[0].data.children[0].data.title);
-        const description = await fetch(json_url).then((response) => response.json()).then((data) => data[0].data.children[0].data.selftext);
-        const upvotes = await fetch(json_url).then((response) => response.json()).then((data) => data[0].data.children[0].data.ups);
-        const percentage = await fetch(json_url).then((response) => response.json()).then((data) => data[0].data.children[0].data.upvote_ratio);
-        return [subreddit, title, description, upvotes, percentage * 100];
+        return await fetch(url + ".json").then((response) => response.json()).then((data) => {
+            //console.log(data[0].data.children[0].data.subreddit);
+            //console.log(data[0].data.children[0].data.title);
+            //console.log(data[0].data.children[0].data.selftext);
+            //console.log(data[0].data.children[0].data.ups);
+            //console.log(data[0].data.children[0].data.upvote_ratio);
+            return [data[0].data.children[0].data.subreddit,
+            data[0].data.children[0].data.title,
+            data[0].data.children[0].data.selftext,
+            data[0].data.children[0].data.ups,
+            Math.fround(data[0].data.children[0].data.upvote_ratio * 100)];
+        });
     },
-    
     displayResults: async function (data) {
         if (this.not_first_search || 1) {
-            // DELETE ".grid-layout" and remake it
             const grid_layout = document.querySelector(".grid-layout");
             grid_layout.remove();
             let new_layout = document.createElement("div");
             new_layout.classList.add('grid-layout');
             document.body.appendChild(new_layout);
         }
-
         const grid_layout = document.querySelector(".grid-layout");
         for (let item in data.items) {
-            const site_body = await this.RedditDescription(data.items[item].link).then((data) => data);
+            try {
+                var site_body = await this.RedditDescription(data.items[item].link).then((data) => data);
+            } catch (err) {
+                console.error(err);
+                console.error(site_body);
+            }
             let element = document.createElement("div");
             element.classList.add('grid-item');
             
-            // CLEAN CODE AND ADD UPVOTE AND PERCENTAGE OF UPVOTES!
-
+            // Subreddit Name
             let item_subreddit = document.createElement("h4");
             item_subreddit.innerText = "r/" + site_body[0];
 
+            // Title
             let item_title = document.createElement("h3");
             item_title.innerText = site_body[1]; // GET TITLE FROM REDDIT NOT GOOGLE
+            item_title.classList.add('grid-item-title');
 
+            // Description
+            let item_snippet = document.createElement("p");
+            let first_snippet = site_body[2].slice(0, 250);
+
+            let newline_count = (site_body[2].match(/\n/g) || []).length;
+            let char_count = 200 - newline_count * 20;
+
+            let second_snippet = first_snippet.replace("\n", "<br>") + "...";
+            item_snippet.innerHTML = second_snippet;
+
+          //  console.log(newline_count);
+         //   site_body[2] = site_body[2].replace("\n", "<br>");
+          //  char_count = char_count - newline_count * 100;
+            //item_snippet.innerHTML = site_body[2].slice(0, char_count);
+           // var newline_count = (site_body[2].match(/\n/g) || []).length;
+            
+           // item_snippet.innerText += "...";
+
+            let bottom_item_div = document.createElement("div");
+            bottom_item_div.classList.add('bottom-item-div');
+
+            // Link Icon
             let item_link = document.createElement("a");
             item_link.href = data.items[item].link;
-            item_link.innerText = "Link";
-            
-            let item_snippet = document.createElement("p");
-            
-            if (site_body[2] == "") {
-                item_snippet.innerText = "[No Description]"
-            } else {
-                console.log(site_body[2]);
-                site_body[2] = site_body[2].replace("\n", "<br>");
-                item_snippet.innerHTML = site_body[2].slice(0, 300); //.split(" ").slice(0, 50).join(" ");
-                item_snippet.innerText += "...";
-            }
+            let item_link_icon = document.createElement("i");
+            item_link_icon.classList.add('item-link', 'fa', 'fa-link');
+            item_link.appendChild(item_link_icon);
 
+            // Upvotes
             let item_upvotes = document.createElement("h4");
-            item_upvotes.innerText = site_body[3];
+            item_upvotes.classList.add('item-upvotes');
+            item_upvotes.innerText = site_body[3] + " Upvotes";
 
+            // Percentage of Upvotes
             let item_percentage = document.createElement("h4");
-            item_percentage.innerText = site_body[4];
+            item_percentage.classList.add('item-percentage');
+            item_percentage.innerText = site_body[4] + "%";
 
-        element.appendChild(item_subreddit);
-        element.appendChild(item_title);
-        element.appendChild(item_link);
-        element.appendChild(item_snippet); // fetch from reddit json use the reddit function
-        element.appendChild(item_upvotes); // fetch from reddit json use the reddit function
-        element.appendChild(item_percentage); // fetch from reddit json use the reddit function
-        document.querySelector(".grid-layout").appendChild(element);
+            element.appendChild(item_subreddit);
+            element.appendChild(item_title);
+            element.appendChild(item_snippet);
+            bottom_item_div.appendChild(item_link);
+            bottom_item_div.appendChild(item_upvotes);
+            bottom_item_div.appendChild(item_percentage);
+            element.appendChild(bottom_item_div);
+            document.querySelector(".grid-layout").appendChild(element);
         }
         this.not_first_search = true;
         const search_button = document.querySelector(".search-button");
@@ -83,3 +108,10 @@ let redditsearch = {
 };
 
 document.querySelector(".search-button").addEventListener("click", () => redditsearch.search());
+
+document.querySelector(".search-bar").addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        redditsearch.search()
+    }
+});
